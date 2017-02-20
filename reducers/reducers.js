02bfec907b1cf07f4ff170f3a128399dc80240e1
Your Name <kelvin.lightner@gmail.com>
2017-02-20 15:54:11 -0800

    Import Actions into Reducers

--- reducers/reducers.js
+++ reducers/reducers.js
@@ -1,4 +1,13 @@
 import {
+  USER_WIN,
+  GAME_OVER,
+  INCREMENT_HANGMAN,
+  FETCH_WORD,
+  SET_WORD,
+  SET_FETCH_WORD_ERROR,
+  NEW_LETTER,
+  INSERT_CORRECT_LETTER,
+  INSERT_INCORRECT_LETTER
 } from '../actions/types';
 
 const reducers = {
