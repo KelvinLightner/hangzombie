4c0bb4fb39fbacd08967bc58045e797a2e46f8fe
Your Name <kelvin.lightner@gmail.com>
2017-02-20 15:55:56 -0800

    Add Game State Logic to Reducer

--- reducers/reducers.js
+++ reducers/reducers.js
@@ -17,6 +17,17 @@ const reducers = {
       MAX_WORD_LENGTH: 15
     };
   },
+
+  gameState: (state  = { win: false, over: false }, action) => {
+    switch (action.type) {
+      case USER_WIN:
+        return {...state, win: true};
+      case GAME_OVER:
+        return {...state, over: true};
+      default:
+        return state;
+    }
+  },
 };
 
 export default reducers;
