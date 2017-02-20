0446359b3d5e8ca27c5d459890be6847510f7af9
Your Name <kelvin.lightner@gmail.com>
2017-02-20 14:42:41 -0800

    Action Types Created

---
+++ actions/types.js
@@ -0,0 +1,17 @@
+export const USER_WIN = 'USER_WIN';
+
+export const GAME_OVER = 'GAME_OVER';
+
+export const INCREMENT_HANGMAN = 'INCREMENT_HANGMAN';
+
+export const FETCH_WORD = 'FETCH_WORD';
+
+export const SET_WORD = 'SET_WORD';
+
+export const SET_FETCH_WORD_ERROR = 'SET_FETCH_WORD_ERROR';
+
+export const NEW_LETTER = 'NEW_LETTER';
+
+export const INSERT_CORRECT_LETTER = 'INSERT_CORRECT_LETTER';
+
+export const INSERT_INCORRECT_LETTER = 'INSERT_INCORRECT_LETTER';
