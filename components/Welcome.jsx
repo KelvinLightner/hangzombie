42177be7d0624c2c24b3d21dc8476e53068f9403
Your Name <kelvin.lightner@gmail.com>
2017-02-20 22:42:02 -0800

    Minor Css Update

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -4,7 +4,7 @@ import AppComponent from './AppComponent.jsx';
 class Welcome extends AppComponent {
   render() {
     return (
-      <div className="welcome">Welcome to HangZombiezzz!!!
+      <div className="welcome">Welcome to HangZombiezz!!!
       	<div className="welcome subheader">Please Guess A Letter to Start the Game</div>
       </div>
     );
